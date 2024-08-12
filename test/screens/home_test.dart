3b618a7f1e3fcd9daaf56c5cd3480b52be703fe5
Elias Ayele <elias@Eliass-MacBook-Pro.local>
2024-08-12 14:14:39 +0300

    testing add fav feature

--- test/screens/home_test.dart
+++ test/screens/home_test.dart
@@ -15,7 +15,7 @@ void main() {
     testWidgets('Testing if ListView shows up', (tester) async {
       await tester.pumpWidget(createHomeScreen());
       expect(find.byType(ListView), findsOneWidget);
-    });
+    }); 
     testWidgets('Testing Scrolling', (tester) async {
       await tester.pumpWidget(createHomeScreen());
       expect(find.text('Item 0'), findsOneWidget);
@@ -26,6 +26,18 @@ void main() {
       );
       await tester.pumpAndSettle();
       expect(find.text('Item 0'), findsNothing);
+    });
+    testWidgets('Testing IconButtons', (tester) async {
+      await tester.pumpWidget(createHomeScreen());
+      expect(find.byIcon(Icons.favorite), findsNothing);
+      await tester.tap(find.byIcon(Icons.favorite_border).first);
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+      expect(find.text('Added to favorites.'), findsOneWidget);
+      expect(find.byIcon(Icons.favorite), findsWidgets);
+      await tester.tap(find.byIcon(Icons.favorite).first);
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+      expect(find.text('Removed from favorites.'), findsOneWidget);
+      expect(find.byIcon(Icons.favorite), findsNothing);
     });
   });
 }
